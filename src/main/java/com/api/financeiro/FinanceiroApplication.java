fcc7744ffd9520775e8beb421e7b0c8089d99749
Pedro Vinicius Mota Barreiro <pedromb.dev@gmail.com>
2023-02-03 23:00:14 -0300

    Arquivo 0

---
+++ src/main/java/com/api/financeiro/FinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.api.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroApplication.class, args);
+	}
+
+}
