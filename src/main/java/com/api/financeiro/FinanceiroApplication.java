ede634fa26f1e76f9f64fc8ef6cbcbaca3cb0021
Pedro Vinicius Mota Barreiro <pedromb.dev@gmail.com>
2023-02-06 19:03:11 -0300

    Dependecia do token adicionada

--- src/main/java/com/api/financeiro/FinanceiroApplication.java
+++ src/main/java/com/api/financeiro/FinanceiroApplication.java
@@ -2,6 +2,7 @@ package com.api.financeiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class FinanceiroApplication {
@@ -9,5 +10,8 @@ public class FinanceiroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceiroApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String index(){
+		return "A cabeça de Adnackson é realmente muito grande";
+	}
 }
